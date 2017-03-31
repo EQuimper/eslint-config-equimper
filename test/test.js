98f889973ecf477d59c2401be541f83fc5d56573
EQuimper <quimperemanuel@gmail.com>
2017-03-30 22:24:52 -0400

    Add travis

---
+++ test/test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const eslint = require('eslint');
+const conf = require('../');
+
+// The source files to lint.
+const repoFiles = [
+  'index.js',
+  'test/test.js',
+];
+
+// Use the rules defined in this repo to test against.
+const eslintOpts = {
+  envs: ['node', 'es6'],
+  useEslintrc: false,
+  rules: conf.rules,
+};
+
+// Runs the linter on the repo files and asserts no errors were found.
+const report = new eslint.CLIEngine(eslintOpts).executeOnFiles(repoFiles);
+assert.equal(report.errorCount, 0);
+assert.equal(report.warningCount, 0);
+repoFiles.forEach((file, index) => {
+  assert(report.results[index].filePath.endsWith(file));
+});
